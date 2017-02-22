6cbe5800307b183cffb0aff2e118f1db46410d04
Christian Fei <crifei93@gmail.com>
2017-02-22 06:37:49 +0100

    separate tests in suites

--- test/index.js
+++ test/index.js
@@ -3,36 +3,38 @@ const huskyTemplate = require('..')
 
 const {equal} = require('assert')
 
-test('returns formatted string', () => {
-  const ipsum = 'ipsum'
-  const text = 'lorem {ipsum}'
-  const template = huskyTemplate(text)
+test('husky-template', () => {
+  test('returns formatted string', () => {
+    const ipsum = 'ipsum'
+    const text = 'lorem {ipsum}'
+    const template = huskyTemplate(text)
 
-  equal('lorem ipsum', template({ipsum}))
-})
+    equal('lorem ipsum', template({ipsum}))
+  })
 
-test('returns curried formatted string', () => {
-  const ipsum = 'ipsum'
-  const amet = 'amet'
-  const text = 'lorem {ipsum} {amet}'
-  const template = huskyTemplate(text)
+  test('returns curried formatted string', () => {
+    const ipsum = 'ipsum'
+    const amet = 'amet'
+    const text = 'lorem {ipsum} {amet}'
+    const template = huskyTemplate(text)
 
-  const curriedTemplate = template({ipsum})
-  equal('lorem ipsum {amet}', curriedTemplate)
-  equal('lorem ipsum amet', curriedTemplate({amet}))
-})
+    const curriedTemplate = template({ipsum})
+    equal('lorem ipsum {amet}', curriedTemplate)
+    equal('lorem ipsum amet', curriedTemplate({amet}))
+  })
 
-test('supports deep object properties', () => {
-  const template = huskyTemplate('lorem ipsum {deep.amet}')
-  const amet = 'amet'
-  equal('lorem ipsum amet', template({deep: {amet}}))
-})
+  test('supports deep object properties', () => {
+    const template = huskyTemplate('lorem ipsum {deep.amet}')
+    const amet = 'amet'
+    equal('lorem ipsum amet', template({deep: {amet}}))
+  })
 
-test('returns partially formatted template', () => {
-  let template = huskyTemplate('lorem ipsum {amet}')
-  equal('lorem ipsum {amet}', template)
+  test('returns partially formatted template', () => {
+    let template = huskyTemplate('lorem ipsum {amet}')
+    equal('lorem ipsum {amet}', template)
 
-  const amet = 'amet'
-  template = template({amet})
-  equal('lorem ipsum amet', template)
+    const amet = 'amet'
+    template = template({amet})
+    equal('lorem ipsum amet', template)
+  })
 })
--- test/utils.js
+++ test/utils.js
@@ -3,8 +3,10 @@ const utils = require('../utils')
 
 const {equal, deepEqual} = require('assert')
 
-test('.lookupKey returns deep property value by lookup notation', () => {
-  const data = {deep: {foo: 'baz'}}
+test('utils', () => {
+  test('.lookupKey returns deep property value by lookup notation', () => {
+    const data = {deep: {foo: 'baz'}}
 
-  equal('baz', utils.lookupKey(data, 'deep.foo'))
+    equal('baz', utils.lookupKey(data, 'deep.foo'))
+  })
 })
